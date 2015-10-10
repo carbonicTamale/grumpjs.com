7d35b542f461d8b6590dbf90d3a6393d484ac786
bkillenit <blaine.j.killen@gmail.com>
2015-10-09 18:16:40 -0700

    grump filter by category works

--- server/client/app/browse/browse.js
+++ server/client/app/browse/browse.js
@@ -1,12 +1,23 @@
 angular.module('grump.browse', [])
 
 .controller('BrowseController', function ($scope, Files) {
+  $scope.allGrumps = [];
   $scope.grumps = [];
+  $scope.categories = [];
   $scope.category = "General";
 
   $scope.getGrumps = function(){
     return Files.getGrumps().then(function (results) {
       $scope.grumps = results;
+      $scope.allGrumps = results;
+
+      for(var i = 0; i < $scope.allGrumps.length; i++) {
+        var grump = $scope.allGrumps[i];
+
+        if($scope.categories.indexOf(grump.category) === -1) {
+          $scope.categories.push(grump.category);
+        }
+      }
 
       //returning scope.grumps for testing...
       return $scope.grumps;
@@ -16,5 +27,17 @@ angular.module('grump.browse', [])
   $scope.filterGrumps = function() {
     // do something with the filter
     // will probably be doing filterng purely in view
+    $scope.grumps = [];
+    console.log($scope.category);
+
+    for(var i = 0; i < $scope.allGrumps.length; i++) {
+      var grump = $scope.allGrumps[i];
+
+      if(grump.category === $scope.category) {
+        $scope.grumps.push(grump);
+      }
+    }
+
+    console.log($scope.grumps);
   }
 });
